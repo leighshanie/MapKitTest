1e85245cfabddd7b38efa6b17aed1fcba5c8ae2b
Shanie Leigh <yao.h.wang@uni.sydney.edu.au>
2020-07-05 19:15:29 +0800

    Initial Commit

---
+++ Shared/MapKitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKitTestApp.swift
+//  Shared
+//
+//  Created by Shane Leigh on 05/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct MapKitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
